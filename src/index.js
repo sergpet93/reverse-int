25cc2d5575702e27ca3fd480002052b3a9fbee4a
sergpet93 <sergpet_93@mail.ru>
2023-01-11 22:23:33 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    var result = '';
+    var array_n = Math.abs(n).toString().split('');
+    for (let i = array_n.length - 1; i >= 0; i--){
+        result += array_n[i];
+    }
+    return Number(result)
 }
